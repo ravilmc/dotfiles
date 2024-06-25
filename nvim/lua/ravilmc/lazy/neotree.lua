7284894bcb02884f51e8935497335cc7cda31afc
Ravi Lamichhane <ravilamichhane30@gmail.com>
2024-06-25 17:21:06 +0545

    LSP for go

--- nvim/lua/ravilmc/lazy/neotree.lua
+++ nvim/lua/ravilmc/lazy/neotree.lua
@@ -1,13 +1,27 @@
 return {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-        "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-        "MunifTanjim/nui.nvim",
-        "3rd/image.nvim",
-    },
-    config = function()
-        vim.keymap.set("n", "<leader>n", ":Neotree filesystem toggle left<CR>")
-    end,
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+		"MunifTanjim/nui.nvim",
+		"3rd/image.nvim",
+	},
+	config = function()
+		vim.keymap.set("n", "<leader>n", ":Neotree filesystem toggle left<CR>")
+
+		require("neo-tree").setup({
+			filesystem = {
+				filtered_items = {
+                    hide_dotfiles = false,
+					hide_by_name = {
+						"bin",
+						"tmp",
+                        "node_modules",
+                        ".git",
+					},
+				},
+			},
+		})
+	end,
 }
--- nvim/lua/ravilmc/lazy/nonels.lua
+++ nvim/lua/ravilmc/lazy/nonels.lua
@@ -1,17 +1,33 @@
 return {
-    "nvimtools/none-ls.nvim",
+	"nvimtools/none-ls.nvim",
 
-    config = function()
-        local null_ls = require("null-ls")
-        null_ls.setup({
-            sources = {
-                null_ls.builtins.formatting.stylua,
-                null_ls.builtins.formatting.prettier,
-                null_ls.builtins.formatting.gofumpt,
-                null_ls.builtins.code_actions.refactoring,
-            },
-        })
+	config = function()
+		local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+		local null_ls = require("null-ls")
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.formatting.prettier,
+				null_ls.builtins.formatting.gofumpt,
+				null_ls.builtins.code_actions.refactoring,
+			},
+			on_attach = function(client, bufnr)
+				if client.supports_method("textDocument/formatting") then
+					vim.api.nvim_clear_autocmds({
+						group = augroup,
+						buffer = bufnr,
+					})
+					vim.api.nvim_create_autocmd("BufWritePre", {
+						group = augroup,
+						buffer = bufnr,
+						callback = function()
+							vim.lsp.buf.format()
+						end,
+					})
+				end
+			end,
+		})
 
-        vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-    end,
+		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+	end,
 }
